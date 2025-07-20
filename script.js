0c0c87a3fd6c0ab34bf95696e683b2bcfb50cc05
Rakshit468 <rakshit4682@gmal.com>
2025-07-20 19:44:31 +0530

    feat: Add a prompt to ask Grid dimensions
    Prompts user for the grid dimensions and creates the
    grid accordingly.

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 256; i++) {
-  let div = document.createElement("div");
-  div.style.width = "31.25px";
-  div.style.height = "31.25px";
-  div.className = "square";
-  container.appendChild(div);
-}
+
+const createGrid = (size) => {
+  if (document.querySelectorAll(".square").length > 0) {
+    document.querySelectorAll(".square").forEach((square) => square.remove());
+  }
+  for (let i = 0; i < size * size; i++) {
+    let div = document.createElement("div");
+    div.style.width = `${1000 / size}px`;
+    div.style.height = `${1000 / size}px`;
+    div.className = "square";
+    container.appendChild(div);
+  }
+  const squares = document.querySelectorAll(".square");
+  console.log(squares.length);
+
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = `rgb(${getRandomInt(
+        0,
+        256
+      )} ${getRandomInt(0, 256)} ${getRandomInt(0, 256)})`;
+    });
+  });
+};
+
+createGrid(20);
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -13,16 +32,13 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const randomExclusiveMax = getRandomInt(0, 10);
-console.log(randomExclusiveMax); // e.g., 4
-const squares = document.querySelectorAll(".square");
-console.log(squares.length);
-
-squares.forEach((square) => {
-  square.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = `rgb(${getRandomInt(
-      0,
-      256
-    )} ${getRandomInt(0, 256)} ${getRandomInt(0, 256)})`;
-  });
-});
+try {
+  let gridDim = prompt("Enter the number of rows of the grid between 20 - 100");
+  if (gridDim < 20 || gridDim > 100 || gridDim == "") {
+    alert("Invalid input! Please enter a valid value ;)");
+  } else {
+    createGrid(gridDim);
+  }
+} catch (error) {
+  console.log(error);
+}
