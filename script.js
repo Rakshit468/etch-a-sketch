20cdc15ec65826bba0cb7ac2df7f3b6b7bd1b962
Rakshit468 <rakshit4682@gmal.com>
2025-07-20 18:20:47 +0530

    feat: Add interactive hover effects to squares
    Implements hover functionality for the grid squares, changing their background
    color to a random RGB value on mouseover

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 255; i++) {
+for (let i = 0; i < 256; i++) {
   let div = document.createElement("div");
-  div.style.width = "27.8px";
-  div.style.height = "27.8px";
-  div.style.border = "1px solid black";
+  div.style.width = "31.25px";
+  div.style.height = "31.25px";
+  div.className = "square";
   container.appendChild(div);
 }
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const randomExclusiveMax = getRandomInt(0, 10);
+console.log(randomExclusiveMax); // e.g., 4
+const squares = document.querySelectorAll(".square");
+console.log(squares.length);
+
+squares.forEach((square) => {
+  square.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = `rgb(${getRandomInt(
+      0,
+      256
+    )} ${getRandomInt(0, 256)} ${getRandomInt(0, 256)})`;
+  });
+});
